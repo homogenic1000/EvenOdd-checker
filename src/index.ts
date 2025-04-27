55bf1546671a0820513a2a5f195e752fed074690
Mathéo Delessert <hello@matheodelssert.ch>
2025-04-28 00:27:38 +0200

    forgot to push the script lol

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Happy developing ✨')
+let UserInput = "576843929238746"
+
+function checkOddEvenInput() {
+     let A = Number(UserInput)
+     let divideA = A/2
+     let  B = divideA.toString();
+     if (B.includes(".")){
+          console.log("it's odd")
+
+     } else {
+          console.log("it's event")
+     }
+}
+
+checkOddEvenInput()
